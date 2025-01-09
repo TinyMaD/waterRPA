78d56a105efba9f883ade3924b06ba7b3ad7afaf
clang <845682630@qq.com>
2025-01-09 14:48:58 +0800

    异常处理

--- waterRPA.py
+++ waterRPA.py
@@ -10,12 +10,16 @@ import pyperclip
 def mouseClick(clickTimes,lOrR,img,reTry):
     if reTry == 1:
         while True:
-            location=pyautogui.locateCenterOnScreen(img,confidence=0.9)
-            if location is not None:
-                pyautogui.click(location.x,location.y,clicks=clickTimes,interval=0.2,duration=0.2,button=lOrR)
-                break
-            print("未找到匹配图片,0.1秒后重试")
-            time.sleep(0.1)
+            try:
+                location=pyautogui.locateCenterOnScreen(img,confidence=0.9)
+                if location is not None:
+                    pyautogui.click(location.x,location.y,clicks=clickTimes,interval=0.2,duration=0.2,button=lOrR)
+                    break
+                print("未找到匹配图片,0.1秒后重试")
+                time.sleep(0.1)
+            except pyautogui.ImageNotFoundException:
+                print("未找到匹配图片,0.1秒后重试")
+                time.sleep(0.1)
     elif reTry == -1:
         while True:
             location=pyautogui.locateCenterOnScreen(img,confidence=0.9)
